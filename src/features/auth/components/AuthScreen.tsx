efcfe16955997ba84c7f2b9d10182d47e5925d14
jay.yang <858851768@qq.com>
2024-12-29 20:48:17 +0800

    🌈 style:

--- src/features/auth/components/AuthScreen.tsx
+++ src/features/auth/components/AuthScreen.tsx
@@ -1,5 +1,9 @@
 "use client"
 
+import { useState } from 'react';
+import { SingFlow } from '../types';
+
 export const AuthScreen: React.FC = () => {
-    return <div className='__AuthScreen'>AuthScreen component works!</div>;
+    const [state, setState] = useState<SingFlow>('singIn');
+    return <div className='h-full flex items-center justify-center bg-[#5C3B58]'>AuthScreen component works!</div>;
 };
---
+++ src/features/auth/types.ts
@@ -0,0 +1,1 @@
+export type SingFlow = 'singIn' | 'signUp';
