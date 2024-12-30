f06bbb5f8c72e4772f4b4e93ebd769aec08af93d
jay.yang <858851768@qq.com>
2024-12-30 16:50:45 +0800

    🐳 chore: password auth

--- src/features/auth/components/SignInCard.tsx
+++ src/features/auth/components/SignInCard.tsx
@@ -7,6 +7,7 @@ import { Separator } from '@/components/ui/separator';
 import { SignFlow } from '../types';
 import { useState } from 'react';
 import { useAuthActions } from '@convex-dev/auth/react';
+import { TriangleAlert } from 'lucide-react';
 
 export interface SignInCardProps {
     setState: (state: SignFlow) => void;
@@ -18,6 +19,7 @@ export const SignInCard: React.FC<SignInCardProps> = (props: SignInCardProps) =>
     const [email, setEmail] = useState<string>('');
     const [spinning, setSpinning] = useState(false);
     const [password, setPassword] = useState<string>('');
+    const [error, setError] = useState<string>('');
 
     const onProviderSignIn = (value: "github" | "google") => {
         setSpinning(true);
@@ -26,13 +28,27 @@ export const SignInCard: React.FC<SignInCardProps> = (props: SignInCardProps) =>
         });
     };
 
+    const onPasswordSignIn = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        setSpinning(true);
+        signIn('password', { email, password, flow: 'signIn' }).catch(() => {
+            setError('invalid email or password');
+        }).finally(() => {
+            setSpinning(false);
+        });
+    }
+
     return <Card className='__SignInCard w-full h-full p-8'>
         <CardHeader className='px-0 pt-0'>
             <CardTitle>Login to continue</CardTitle>
             <CardDescription>Use your email or another service</CardDescription>
         </CardHeader>
+        {!!error && <div className='bg-destructive/15 p-3 rounded-md flex items-center gap-x-2 text-sm text-destructive mb-6'>
+            <TriangleAlert className='size-4'></TriangleAlert>
+            <p>{error}</p>
+        </div>}
         <CardContent className='space-y-5 px-0 pb-0'>
-            <form className='space-y-2.5'>
+            <form onSubmit={onPasswordSignIn} className='space-y-2.5'>
                 <Input disabled={spinning} value={email} placeholder='Email' type='email' onChange={(e) => {setEmail(e.target.value)}} required />
                 <Input disabled={spinning} value={password} placeholder='Password' type='password' onChange={(e) => {setPassword(e.target.value)}} required />
                 <Button type='submit' className='w-full' size={'lg'} disabled={spinning}>Continue</Button>
@@ -40,7 +56,6 @@ export const SignInCard: React.FC<SignInCardProps> = (props: SignInCardProps) =>
             <Separator />
             <div className='flex flex-col gap-y-2.5'>
                 <Button variant='outline' className='w-full' size={'lg'} disabled={spinning} onClick={() => onProviderSignIn('github')}><FaGithub /> Continue with Github</Button>
-                <Button variant='outline' className='w-full' size={'lg'} disabled><FcGoogle /> Continue with Google</Button>
             </div>
             <p className='text-xs text-muted-foreground'>
                 Don&apos;t have an account? <span className='text-sky-700 hover:underline cursor-pointer'
