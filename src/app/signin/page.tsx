78f6f79eb31531eaae24fab337429b9ff28ae3ee
jay.yang <858851768@qq.com>
2024-12-30 13:57:42 +0800

    🐳 chore:

---
+++ src/app/signin/page.tsx
@@ -0,0 +1,5 @@
+const Page: React.FC = () => {
+    return <div className='__page'>page component works!</div>;
+};
+
+export default Page;
---
+++ src/app/test/page.tsx
@@ -0,0 +1,5 @@
+const Page: React.FC = () => {
+    return <div className='__page'>test page component works!</div>;
+};
+
+export default Page;
