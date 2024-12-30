66b6925ecfee9f60fa65700cbe60862eae07d758
jay.yang <858851768@qq.com>
2024-12-30 16:30:20 +0800

    🐳 chore:

--- src/features/auth/components/SignInCard.tsx
+++ src/features/auth/components/SignInCard.tsx
@@ -16,10 +16,14 @@ export const SignInCard: React.FC<SignInCardProps> = (props: SignInCardProps) =>
     const { setState } = props;
     const { signIn } = useAuthActions();
     const [email, setEmail] = useState<string>('');
+    const [spinning, setSpinning] = useState(false);
     const [password, setPassword] = useState<string>('');
 
-    const handleProviderSignIn = (value: "github" | "google") => {
-        signIn(value);
+    const onProviderSignIn = (value: "github" | "google") => {
+        setSpinning(true);
+        signIn(value).finally(() => {
+            setSpinning(false);
+        });
     };
 
     return <Card className='__SignInCard w-full h-full p-8'>
@@ -29,13 +33,13 @@ export const SignInCard: React.FC<SignInCardProps> = (props: SignInCardProps) =>
         </CardHeader>
         <CardContent className='space-y-5 px-0 pb-0'>
             <form className='space-y-2.5'>
-                <Input disabled={false} value={email} placeholder='Email' type='email' onChange={(e) => {setEmail(e.target.value)}} required />
-                <Input disabled={false} value={password} placeholder='Password' type='password' onChange={(e) => {setPassword(e.target.value)}} required />
-                <Button type='submit' className='w-full' size={'lg'}>Continue</Button>
+                <Input disabled={spinning} value={email} placeholder='Email' type='email' onChange={(e) => {setEmail(e.target.value)}} required />
+                <Input disabled={spinning} value={password} placeholder='Password' type='password' onChange={(e) => {setPassword(e.target.value)}} required />
+                <Button type='submit' className='w-full' size={'lg'} disabled={spinning}>Continue</Button>
             </form>
             <Separator />
             <div className='flex flex-col gap-y-2.5'>
-                <Button variant='outline' className='w-full' size={'lg'} onClick={() => handleProviderSignIn('github')}><FaGithub /> Continue with Github</Button>
+                <Button variant='outline' className='w-full' size={'lg'} disabled={spinning} onClick={() => onProviderSignIn('github')}><FaGithub /> Continue with Github</Button>
                 <Button variant='outline' className='w-full' size={'lg'} disabled><FcGoogle /> Continue with Google</Button>
             </div>
             <p className='text-xs text-muted-foreground'>
