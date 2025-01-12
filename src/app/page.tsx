b941b11ef2ceee14244ac9003e01e738407ea3a3
jay.yang <858851768@qq.com>
2025-01-12 13:58:12 +0800

    🐳 chore:

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,28 @@
 'use client';
 
+import { useEffect, useMemo } from 'react';
+
 import { UserBtn } from '@/features/auth/components/UserBtn';
+import { useGetWorkspaces } from '@/features/workspaces/api/useGetWorkspaces';
 
 export default function Home() {
+  const { isLoadingWorkspaces, workspaces } = useGetWorkspaces();
+  const workspaceId = useMemo(() => {
+    return workspaces?.[0]?._id;
+  }, [workspaces]);
+
+  useEffect(() => {
+    if (isLoadingWorkspaces) {
+      return;
+    }
+    if (!workspaceId) {
+      // TODO: handle no workspace
+      return;
+    } else {
+      // TODO: handle workspace
+    }
+  }, [workspaceId, isLoadingWorkspaces]);
+
   return (
     <div>
       <UserBtn></UserBtn>
