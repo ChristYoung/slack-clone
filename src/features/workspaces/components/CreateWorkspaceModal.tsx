5bd987789ffa41b3bb8a9c012199344e895bc645
jay.yang <858851768@qq.com>
2025-01-12 16:18:40 +0800

    🐳 chore:

--- src/features/workspaces/components/CreateWorkspaceModal.tsx
+++ src/features/workspaces/components/CreateWorkspaceModal.tsx
@@ -4,14 +4,35 @@ import { Button } from '@/components/ui/button';
 import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog';
 import { Input } from '@/components/ui/input';
 
+import { useCreateWorkspaceApi } from '../apis/useCreateWorkspaceApi';
 import { useCreateWorkspaceModal } from '../store/workspaceModal.store';
 
 export interface CreateWorkspaceModalProps {}
 
 export const CreateWorkspaceModal: React.FC<CreateWorkspaceModalProps> = (props: CreateWorkspaceModalProps) => {
   const [open, setOpen] = useCreateWorkspaceModal();
+  const { mutate } = useCreateWorkspaceApi();
   const handleClose = () => {
     setOpen(false);
+  };
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    mutate(
+      {
+        name: 'WorkSpace1',
+      },
+      {
+        onSuccess: () => {
+          // redirect to the workspace Id
+        },
+        onError: (error) => {
+          // Show error toast
+        },
+        onSettled: () => {
+          // Reset Form
+        },
+      }
+    );
   };
   return (
     <Dialog open={open} onOpenChange={handleClose}>
