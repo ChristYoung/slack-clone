dd15ab91341b8ef7dc8d2ab8cd98b4db9339141e
jay.yang <858851768@qq.com>
2025-01-12 15:41:55 +0800

    🐳 chore:

--- convex/workspaces.ts
+++ convex/workspaces.ts
@@ -1,4 +1,26 @@
-import { query } from './_generated/server';
+import { getAuthUserId } from '@convex-dev/auth/server';
+import { v } from 'convex/values';
+
+import { mutation, query } from './_generated/server';
+
+const create = mutation({
+  args: {
+    name: v.string(),
+  },
+  handler: async (ctx, { name }) => {
+    const userId = await getAuthUserId(ctx);
+    if (!userId) {
+      throw new Error('User not authenticated');
+    }
+    const joinCode = 'dsdsdsds1222';
+    const workspaceId = await ctx.db.insert('workspaces', {
+      name,
+      joinCode,
+      userId,
+    });
+    return workspaceId;
+  },
+});
 
 export const get = query({
   args: {},
