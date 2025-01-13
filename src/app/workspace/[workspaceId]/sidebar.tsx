bcbcff4a66b802ff4e3d70684e1173be18938c59
jay.yang <858851768@qq.com>
2025-01-13 16:56:03 +0800

    🌈 style:

--- src/app/workspace/[workspaceId]/sidebar.tsx
+++ src/app/workspace/[workspaceId]/sidebar.tsx
@@ -1,7 +1,10 @@
 'use client';
 
+import { Home } from 'lucide-react';
+
 import { UserBtn } from '@/features/auth/components/UserBtn';
 
+import SidebarBtn from './sidebarBtn';
 import WorkspaceSwitcher from './workspaceSwitcher';
 
 export interface SidebarProps {}
@@ -10,6 +13,7 @@ const Sidebar: React.FC<SidebarProps> = (props: SidebarProps) => {
   return (
     <aside className='__sidebar w-[70px] h-full bg-[#481349] flex flex-col gap-y-4 items-center pt-[9px] pb-4 pr-3 pl-3'>
       <WorkspaceSwitcher />
+      <SidebarBtn icon={Home} label='Home' active />
       <div className='flex flex-col items-center justify-center gap-y-1 mt-auto'>
         <UserBtn />
       </div>
---
+++ src/app/workspace/[workspaceId]/sidebarBtn.tsx
@@ -0,0 +1,25 @@
+import { LucideIcon } from 'lucide-react';
+import { IconType } from 'react-icons/lib';
+
+import { Button } from '@/components/ui/button';
+import { cn } from '@/lib/utils';
+
+export interface SidebarBtnProps {
+  icon: LucideIcon | IconType;
+  label: string;
+  active: boolean;
+}
+
+const SidebarBtn: React.FC<SidebarBtnProps> = (props: SidebarBtnProps) => {
+  const { active, icon: Icon, label } = props;
+  return (
+    <div className='__sidebarBtn flex flex-col items-center justify-center gap-y-0.5 cursor-pointer group'>
+      <Button variant={'transparent'} className={cn('size-9 p-2 group-hover:bg-accent/20', active && 'bg-accent/20')}>
+        <Icon className='size-5 text-white group-hover:scale-110 transition-all duration-300' />
+      </Button>
+      <span className='text-[11px] text-white group-hover:text-accent'>{label}</span>
+    </div>
+  );
+};
+
+export default SidebarBtn;
