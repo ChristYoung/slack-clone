9d066a533ab162644c9070930aa98d9af0ba4991
jay.yang <858851768@qq.com>
2025-01-13 12:37:28 +0800

    🐳 chore: 3:05:21 and finished chapter `toolbar`

--- src/app/workspace/[workspaceId]/toolbar.tsx
+++ src/app/workspace/[workspaceId]/toolbar.tsx
@@ -1,21 +1,25 @@
 import { Search, Info } from 'lucide-react';
 
 import { Button } from '@/components/ui/button';
+import { useGetWorkspaceByIdApi } from '@/features/workspaces/apis/useGetWorkspaceByIdApi';
+import { useWorkSpaceId } from '@/hooks/useWorkSpaceId';
 
 export interface ToolbarProps {}
 
 const Toolbar: React.FC<ToolbarProps> = (props: ToolbarProps) => {
+  const workspaceId = useWorkSpaceId();
+  const { workspaceItem } = useGetWorkspaceByIdApi({ id: workspaceId });
   return (
     <nav className='__toolbar bg-[#481349] flex items-center justify-between h-10 p-1.5 text-white'>
       <div className='flex-1' />
       <div className='min-w-[280px] max-w-[642px] grow-[2] shrink'>
         <Button size={'sm'} className='bg-accent/25 hover:bg-accent/25 w-full justify-start h-7 px-2'>
           <Search className='size-4 text-white mr-2' />
-          <span className='text-white text-xs'>Search Workspaces</span>
+          <span className='text-white text-xs'>Search {workspaceItem?.name}</span>
         </Button>
       </div>
       <div className='ml-auto flex flex-1 items-center justify-end'>
-        <Button variant={'ghost'} className='bg-accent/25 hover:bg-accent/25'>
+        <Button variant={'transparent'} size={'iconSm'}>
           <Info className='size-5 text-white' />
         </Button>
       </div>
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -16,12 +16,14 @@ const buttonVariants = cva(
         ghost: 'hover:bg-accent hover:text-accent-foreground',
         link: 'text-primary underline-offset-4 hover:underline',
         slack: 'bg-emerald-500 text-white hover:bg-emerald-500/80',
+        transparent: 'bg-transparent hover:bg-accent/10 text-accent',
       },
       size: {
         default: 'h-10 px-4 py-2',
-        sm: 'h-9 rounded-md px-3',
+        sm: 'h-8 rounded-md px-3',
         lg: 'h-11 rounded-md px-8',
         icon: 'h-10 w-10',
+        iconSm: 'h-8 w-8',
       },
     },
     defaultVariants: {
@@ -31,14 +33,18 @@ const buttonVariants = cva(
   }
 );
 
-export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement>, VariantProps<typeof buttonVariants> {
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
   asChild?: boolean;
 }
 
-const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(({ className, variant, size, asChild = false, ...props }, ref) => {
-  const Comp = asChild ? Slot : 'button';
-  return <Comp className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...props} />;
-});
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, variant, size, asChild = false, ...props }, ref) => {
+    const Comp = asChild ? Slot : 'button';
+    return <Comp className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...props} />;
+  }
+);
 Button.displayName = 'Button';
 
 export { Button, buttonVariants };
