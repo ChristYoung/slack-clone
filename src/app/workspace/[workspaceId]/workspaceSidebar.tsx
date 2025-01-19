f2be250db504f9db82417ada5763b81df4474c39
jay.yang <858851768@qq.com>
2025-01-19 11:10:03 +0800

    🐳 chore:

--- src/app/workspace/[workspaceId]/workspaceSidebar.tsx
+++ src/app/workspace/[workspaceId]/workspaceSidebar.tsx
@@ -1,5 +1,12 @@
+import { useCurrentMemberApi } from '@/features/members/api/useCurrentMemberApi';
+import { useGetWorkspaceByIdApi } from '@/features/workspaces/apis/useGetWorkspaceByIdApi';
+import { useWorkSpaceId } from '@/hooks/useWorkSpaceId';
+
 export interface WorkspaceSidebarProps {}
 
 export const WorkspaceSidebar: React.FC<WorkspaceSidebarProps> = (props: WorkspaceSidebarProps) => {
+  const workspaceId = useWorkSpaceId();
+  const { data: currentMember, isLoading: isCurrentMemberLoading } = useCurrentMemberApi({ workspaceId });
+  const { workspaceItem, isLoadingWorkspace } = useGetWorkspaceByIdApi({ id: workspaceId });
   return <div className='__workspaceSidebar'>workspaceSidebar component works!</div>;
 };
--- src/features/members/api/useCurrentMemberApi.ts
+++ src/features/members/api/useCurrentMemberApi.ts
@@ -4,7 +4,7 @@ import { useEffect, useState } from 'react';
 import { api } from '../../../../convex/_generated/api';
 import { Id } from '../../../../convex/_generated/dataModel';
 
-export const useCurrentMember = ({ workspaceId }: { workspaceId: Id<'workspaces'> }) => {
+export const useCurrentMemberApi = ({ workspaceId }: { workspaceId: Id<'workspaces'> }) => {
   const data = useQuery(api.members.current, { workspaceId });
   const isLoading = data === undefined;
   return { data, isLoading };
