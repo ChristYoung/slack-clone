035fb2f9c3d7940825f745fb39d8288451ac8aa3
jay.yang <858851768@qq.com>
2025-01-19 20:18:20 +0800

    🌈 style:

--- src/app/workspace/[workspaceId]/workspaceHeader.tsx
+++ src/app/workspace/[workspaceId]/workspaceHeader.tsx
@@ -1,3 +1,4 @@
+import { DropdownMenuSeparator } from '@radix-ui/react-dropdown-menu';
 import { ChevronDown } from 'lucide-react';
 
 import { Button } from '@/components/ui/button';
@@ -19,11 +20,33 @@ export const WorkspaceHeader: React.FC<{ workspaceItem: Doc<'workspaces'> }> = (
     <div className='__workspaceHeader flex items-center justify-between px-4 h-[49px] gap-0.5'>
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
-          <Button variant={'transparent'} className='font-semibold text-lg w-auto p-1.5 overflow-hidden' size={'sm'}>
+          <Button
+            variant={'transparent'}
+            className='font-semibold outline-none text-lg w-auto p-1.5 overflow-hidden'
+            size={'sm'}
+          >
             <span className='truncate max-w-[200px]'>{workspaceItem?.name}</span>
             <ChevronDown className='size-4 ml-1 shrink-0' />
           </Button>
         </DropdownMenuTrigger>
+        <DropdownMenuContent align='start' side='bottom' className='w-64'>
+          <DropdownMenuItem className='cursor-pointer capitalize' key={workspaceItem?._id}>
+            <div className='size-9 relative overflow-hidden bg-[#616061] text-white font-semibold text-xl rounded-md flex items-center justify-center'>
+              {workspaceItem?.name.charAt(0).toUpperCase()}
+            </div>
+            <div className='flex flex-col items-start'>
+              <p className='font-bold'>{workspaceItem.name}</p>
+              <p className='text-xs text-muted-foreground'>Active workspace</p>
+            </div>
+          </DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem className='cursor-pointer py-2' onClick={() => {}}>
+            Invite people to {workspaceItem.name}
+          </DropdownMenuItem>
+          <DropdownMenuItem className='cursor-pointer py-2' onClick={() => {}}>
+            Preferences
+          </DropdownMenuItem>
+        </DropdownMenuContent>
       </DropdownMenu>
     </div>
   );
