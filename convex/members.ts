5a56bc47df4633e76f9fb1eb291fda1437d01abf
jay.yang <858851768@qq.com>
2025-01-19 10:48:03 +0800

    🐳 chore:

---
+++ convex/members.ts
@@ -0,0 +1,18 @@
+import { getAuthUserId } from '@convex-dev/auth/server';
+import { v } from 'convex/values';
+
+import { mutation, query } from './_generated/server';
+
+export const current = query({
+  args: { workspaceId: v.id('workspaces') },
+  handler: async (ctx, args) => {
+    const userId = await getAuthUserId(ctx);
+    if (!userId) {
+      return null;
+    }
+    const member = await ctx.db
+      .query('members')
+      .withIndex('by_workspace_id_and_user_id', (q) => q.eq('workspaceId', args.workspaceId).eq('userId', userId))
+      .collect();
+  },
+});
