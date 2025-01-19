65e05206b895ace8f787fdd55fa308a27757414c
jay.yang <858851768@qq.com>
2025-01-19 20:52:03 +0800

    🌈 style:

--- src/app/workspace/[workspaceId]/workspaceHeader.tsx
+++ src/app/workspace/[workspaceId]/workspaceHeader.tsx
@@ -1,4 +1,4 @@
-import { ChevronDown } from 'lucide-react';
+import { ChevronDown, ListFilter, SquarePen } from 'lucide-react';
 
 import { Button } from '@/components/ui/button';
 import {
@@ -49,6 +49,14 @@ export const WorkspaceHeader: React.FC<WorkspaceHeaderProps> = ({ workspaceItem,
           )}
         </DropdownMenuContent>
       </DropdownMenu>
+      <div className='flex items-center gap-0.5'>
+        <Button variant={'transparent'} size={'iconSm'}>
+          <ListFilter className='size-4' />
+        </Button>
+        <Button variant={'transparent'} size={'iconSm'}>
+          <SquarePen className='size-4' />
+        </Button>
+      </div>
     </div>
   );
 };
