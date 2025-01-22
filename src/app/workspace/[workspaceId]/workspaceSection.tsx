344dcf2e2834b51d53750a880abed6c8fcca89de
jay.yang <858851768@qq.com>
2025-01-22 22:37:23 +0800

    🌈 style:

--- src/app/workspace/[workspaceId]/workspaceSection.tsx
+++ src/app/workspace/[workspaceId]/workspaceSection.tsx
@@ -1,3 +1,7 @@
+import { FaCaretDown } from 'react-icons/fa';
+
+import { Button } from '@/components/ui/button';
+
 export interface WorkspaceSectionProps {
   label: string;
   hint: string;
@@ -7,5 +11,21 @@ export interface WorkspaceSectionProps {
 
 export const WorkspaceSection: React.FC<WorkspaceSectionProps> = (props: WorkspaceSectionProps) => {
   const { label, hint, children, onNew } = props;
-  return <div className='__workspaceSection flex flex-col mt-3'>{children}</div>;
+  return (
+    <div className='__workspaceSection flex flex-col mt-3'>
+      <div className='flex items-center px-3.5 group'>
+        <Button variant={'transparent'} className='p-0.5 text-sm text-[#f9edffcc] shrink-0 size-6'>
+          <FaCaretDown className='size-4' />
+        </Button>
+        <Button
+          variant={'transparent'}
+          size={'sm'}
+          className='group px-1.5 text-sm text-[#f9edffcc] h-[28px] justify-start overflow-hidden items-center'
+        >
+          <span className='text-sm font-semibold'>{label}</span>
+        </Button>
+      </div>
+      {children}
+    </div>
+  );
 };
