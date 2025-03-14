24236b88f20fdb286a1e0c8862f49a1e4ad4a0ca
jay.yang <858851768@qq.com>
2025-03-14 16:31:12 +0800

    chore: create a channel- 5:53:55

--- src/features/channels/components/CreateChannelModal.tsx
+++ src/features/channels/components/CreateChannelModal.tsx
@@ -3,7 +3,9 @@ import { useState } from 'react';
 import { Button } from '@/components/ui/button';
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from '@/components/ui/dialog';
 import { Input } from '@/components/ui/input';
+import { useWorkSpaceId } from '@/hooks/useWorkSpaceId';
 
+import { useCreateChannelApi } from '../apis/useCreateChannelApi';
 import { useCreateChannelModal } from '../store/useCreateChannelModal';
 
 export interface CreateChannelModalProps {}
@@ -11,8 +13,10 @@ export interface CreateChannelModalProps {}
 export const CreateChannelModal: React.FC<CreateChannelModalProps> = (
   props: CreateChannelModalProps
 ) => {
+  const workspaceId = useWorkSpaceId();
   const [open, setOpen] = useCreateChannelModal();
   const [channelName, setChannelName] = useState<string>();
+  const { mutate, isPending } = useCreateChannelApi();
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value.replace(/\s+/g, '-').toLowerCase();
     setChannelName(value);
@@ -21,13 +25,26 @@ export const CreateChannelModal: React.FC<CreateChannelModalProps> = (
     setOpen(false);
     setChannelName('');
   };
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!channelName) return;
+    await mutate(
+      { name: channelName, workspaceId },
+      {
+        onSuccess: () => {
+          handClose();
+          // TODO: Redirect to the new channel
+        },
+      }
+    );
+  };
   return (
     <Dialog open={open} onOpenChange={handClose}>
       <DialogContent>
         <DialogHeader>
           <DialogTitle>Create a Channel</DialogTitle>
         </DialogHeader>
-        <form>
+        <form className='space-y-4' onSubmit={handleSubmit}>
           <Input
             value={channelName}
             disabled={false}
@@ -38,8 +55,8 @@ export const CreateChannelModal: React.FC<CreateChannelModalProps> = (
             placeholder='Channel Name'
             maxLength={100}
           />
-          <div className='flex justify-end mt-5'>
-            <Button disabled={false}>Create</Button>
+          <div className='flex justify-end'>
+            <Button disabled={isPending}>Create</Button>
           </div>
         </form>
       </DialogContent>
