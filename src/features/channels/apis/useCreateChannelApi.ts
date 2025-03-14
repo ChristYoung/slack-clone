09d581a3613b988ce7521933a2d6957baa3a00d4
jay.yang <858851768@qq.com>
2025-03-14 16:24:45 +0800

    chore: create channel api

---
+++ src/features/channels/apis/useCreateChannelApi.ts
@@ -0,0 +1,45 @@
+import { useMutation } from 'convex/react';
+import { useCallback, useMemo, useState } from 'react';
+
+import { api } from '../../../../convex/_generated/api';
+import { Id } from '../../../../convex/_generated/dataModel';
+
+type Options = {
+  onSuccess?: (data?: Id<'channels'>) => void;
+  onError?: (error?: Error) => void;
+  onSettled?: () => void;
+};
+
+type RequestProp = { name: string; workspaceId: Id<'workspaces'> };
+
+export const useCreateChannelApi = () => {
+  const [workSpaceId, setWorkSpaceId] = useState<Id<'channels'> | null>(null);
+  const [status, setStatus] = useState<'settled' | 'pending' | 'success' | 'error' | null>(null);
+  const isPending = useMemo(() => status === 'pending', [status]);
+  const isSuccess = useMemo(() => status === 'success', [status]);
+  const isError = useMemo(() => status === 'error', [status]);
+  const settled = useMemo(() => status === 'settled', [status]);
+
+  const mutation = useMutation(api.channels.create);
+  const mutate = useCallback(
+    async (requestBody: RequestProp, options?: Options) => {
+      try {
+        setStatus('pending');
+        setWorkSpaceId(null);
+        const response = await mutation(requestBody);
+        setWorkSpaceId(response);
+        setStatus('success');
+        options?.onSuccess?.(response);
+        return response;
+      } catch (error) {
+        setStatus('error');
+        options?.onError?.(error as Error);
+      } finally {
+        setStatus('settled');
+        options?.onSettled?.();
+      }
+    },
+    [mutation]
+  );
+  return { mutate, workSpaceId, isPending, isSuccess, isError, settled };
+};
