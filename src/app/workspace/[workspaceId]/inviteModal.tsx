4817f671c67b218a047ec4170aaf4d44ac3f3f9a
jay.yang <858851768@qq.com>
2025-03-16 15:54:28 +0800

    chore:

--- src/app/workspace/[workspaceId]/inviteModal.tsx
+++ src/app/workspace/[workspaceId]/inviteModal.tsx
@@ -1,8 +1,12 @@
 'use client';
 
 import { DialogDescription } from '@radix-ui/react-dialog';
+import { CopyIcon } from 'lucide-react';
+import { toast, Toaster } from 'sonner';
 
+import { Button } from '@/components/ui/button';
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from '@/components/ui/dialog';
+import { useWorkSpaceId } from '@/hooks/useWorkSpaceId';
 
 export interface InviteModalProps {
   open: boolean;
@@ -13,6 +17,14 @@ export interface InviteModalProps {
 
 export const InviteModal: React.FC<InviteModalProps> = (props: InviteModalProps) => {
   const { open, setOpen, name, joinCode } = props;
+  const workspaceId = useWorkSpaceId();
+  const handleCopy = () => {
+    const inviteLink = `${window.location.origin}/join/${workspaceId}`;
+    navigator.clipboard.writeText(inviteLink).then(() => {
+      toast.success('Link copied to clipboard');
+    });
+  };
+
   return (
     <Dialog open={open} onOpenChange={setOpen}>
       <DialogContent>
@@ -24,6 +36,10 @@ export const InviteModal: React.FC<InviteModalProps> = (props: InviteModalProps)
         </DialogHeader>
         <div className='flex flex-col items-center justify-center py-10 gap-y-4'>
           <p className='text-4xl font-bold tracking-widest uppercase'>{joinCode}</p>
+          <Button variant={'ghost'} size='sm' onClick={handleCopy}>
+            Copy Link
+            <CopyIcon className='size-4 ml-2' />
+          </Button>
         </div>
       </DialogContent>
     </Dialog>
